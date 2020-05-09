3747dedb4a5292c2cbfab3f84c57d63bdbe8daab
jmauro33 <jonathanmauro16@yahoo.com>
2020-05-09 13:16:26 -0500

    trying to figure out backend

--- src/components/card.js
+++ src/components/card.js
@@ -2,7 +2,8 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div className="card text-center">
+
+    <div className="moviecard text-center">
       <div className="card-header">
         <h2>{props.heading}</h2>
       </div>
