f40703958b26e2bf98b73aa7ad3ca26362d8c1e6
jmauro33 <jonathanmauro16@yahoo.com>
2020-05-11 12:10:49 -0500

    trying to get controllers setup

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 
-// Defining methods for the movieController
+
 module.exports = {
 
 };
--- src/routes/api/moviepost.js
+++ src/routes/api/moviepost.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 const movieController = require("../../controllers/movieController");
 
-// Matches with "/api/posts"
+
 router
   .route("/")
   .get(movieController.findAll)
   .post(movieController.create);
 
-// Matches with "/api/posts/:id"
 router
   .route("/:id")
   .get(movieController.findById)
